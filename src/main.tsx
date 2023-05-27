9e0ad11e625d1dbb796130f24033c353fc27b4eb
james <james@mercstudio.com>
2023-05-27 22:32:59 +0800

    react 18 fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,17 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
