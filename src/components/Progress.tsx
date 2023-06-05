d7f78ca50d5c0063352a2a696a5272e9035c7556
james <james@mercstudio.com>
2023-06-05 13:20:46 +0800

    pulsing progress

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -17,9 +17,8 @@ const Progress = ({ playing, progress, className }: Props) => {
       <Box
         h="100%"
         transition="all 1s ease-in-out"
-        animation={{ scale: '1, 1.1, 1', opacity: '1, 0.5, 1' }}
         // animate-pulse
-        className={`progress__bar ${playing ? ' ' : ''}`}
+        className={`progress__bar ${playing ? ' animate-pulse' : ''}`}
         style={{ width: `${progress}%`, backgroundColor: '#FF5900' }}
       ></Box>
     </Box>
